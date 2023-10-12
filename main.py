8d8992174cab4938ec971654b7b58eb1d091b181
Slimcent <centpresley@gmail.com>
2023-10-12 21:46:43 +0100

    Ridge regression algorithm

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# L2 regularization (Ridge) adds the squares of the model's coefficients as a penalty term to the cost function.
 
+# Sample data
+data = {
+    'X1': [1, 2, 3, 4, 5],
+    'X2': [3, 4, 2, 5, 1],
+    'X3': [7, 5, 10, 8, 6],
+    'y': [10, 20, 25, 30, 40]
+}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+df = pd.DataFrame(data)
 
+# Independent variables (features)
+X = df[['X1', 'X2', 'X3']]
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Dependent variable
+y = df['y']
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create and fit a Ridge regression model
+ridge = Ridge(alpha=0.1)  # You can adjust the regularization strength with the alpha parameter
+ridge.fit(X_train, y_train)
+
+# Make predictions
+y_pred = ridge.predict(X_test)
+
+# Calculate the mean squared error
+mse = mean_squared_error(y_test, y_pred)
+print("Mean Squared Error:", mse)
+
+# Get the model coefficients (including regularization)
+coefficients = ridge.coef_
+intercept = ridge.intercept_
+print("Coefficients:", coefficients)
+print("Intercept:", intercept)
